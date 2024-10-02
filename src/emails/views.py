a9f290f10f7648f6298220bc27b3c92c6a13e77c
s3xmaxman <badside3919@gmail.com>
2024-10-03 06:17:28 +0900

    Enrich the Session With Verified Email ID

--- src/emails/views.py
+++ src/emails/views.py
@@ -1,8 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
 
 from . import services
 
 
 # Create your views here.
 def verify_email_token_view(request, token, *args, **kwargs):
-    did_verify = services.verify_token(token)
+    """
+    トークンを検証し、ユーザーを認証します。
+
+    引数:
+        request (django.http.request.HttpRequest): リクエストオブジェクト。
+        token (str): 検証するトークン。
+
+    戻り値:
+        django.http.response.HttpResponse: トークンが有効な場合は次のURLにリダイレクトし、
+            そうでない場合は/login/にリダイレクトします。
+    """
+    did_verify, msg, email_obj = services.verify_token(token)
+    if not did_verify:
+        try:
+            del request.session["email_id"]
+        except:
+            pass
+        messages.error(request, msg)
+        return redirect("/login/")
+    messages.success(request, msg)
+    request.session["email_id"] = f"{email_obj.id}"
+    next_url = request.session.get("next_url") or "/"
+    if not next_url.startswith("/"):
+        next_url = "/"
+    return redirect(next_url)
