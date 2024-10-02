806a685e1ad04393b3ca3ff467ef8a0e51f5443c
s3xmaxman <badside3919@gmail.com>
2024-10-02 09:39:31 +0900

    Public ID As URL Path Lookups

--- src/cfehome/urls.py
+++ src/cfehome/urls.py
@@ -1,18 +1,18 @@
 """
-URL configuration for cfehome project.
+cfehome プロジェクトの URL 設定
 
-The `urlpatterns` list routes URLs to views. For more information please see:
+`urlpatterns` リストは URL をビューにルーティングします。詳細については、以下を参照してください。
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+例：
+関数ビュー
+    1. インポートを追加:  from my_app import views
+    2. urlpatterns に URL を追加:  path('', views.home, name='home')
+クラスベースビュー
+    1. インポートを追加:  from other_app.views import Home
+    2. urlpatterns に URL を追加:  path('', Home.as_view(), name='home')
+別の URLconf を含める
+    1. include() 関数をインポート: from django.urls import include, path
+    2. urlpatterns に URL を追加:  path('blog/', include('blog.urls'))
 """
 
 from django.conf import settings
