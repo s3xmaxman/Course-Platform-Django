2defe7cea38fdf36eec8397bd1a6de92bab89de9
s3xmaxman <badside3919@gmail.com>
2024-10-02 17:39:34 +0900

    Create the Email Model Form

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
+from django.conf import settings
+
+from emails.forms import EmailForm
+
+EMAIL_ADDRESS = settings.EMAIL_ADDRESS
 
 
 def home_view(request, *args, **kwargs):
     template_name = "home.html"
-    return render(request, template_name)
+    form = EmailForm(request.POST or None)
+    context = {
+        "form": form,
+        "message": "",
+    }
+    if form.is_valid():
+        form.save()
+        context["form"] = EmailForm()
+        context["message"] = (
+            f"Success! Check your email for verification from {EMAIL_ADDRESS}"
+        )
+    return render(request, template_name, context)
