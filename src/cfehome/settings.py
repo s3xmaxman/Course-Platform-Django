a790c8de639945ac47e14f1bbddbd94e70e24967
s3xmaxman <badside3919@gmail.com>
2024-10-02 16:58:23 +0900

    Configure Django to Send Email with Gmail

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -13,6 +13,25 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 from decouple import config
 
+EMAIL_HOST = config("EMAIL_HOST", cast=str, default=None)
+EMAIL_PORT = config("EMAIL_PORT", cast=str, default="587")  # Recommended
+EMAIL_ADDRESS = "hungrypy@gmail.com"
+EMAIL_HOST_USER = config("EMAIL_HOST_USER", cast=str, default=None)
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", cast=str, default=None)
+EMAIL_USE_TLS = config(
+    "EMAIL_USE_TLS", cast=bool, default=True
+)  # Use EMAIL_PORT 587 for TLS
+
+ADMIN_USER_NAME = config("ADMIN_USER_NAME", default="Bad")
+ADMIN_USER_EMAIL = config("ADMIN_USER_EMAIL", default=None)
+
+MANAGERS = []
+ADMINS = []
+if all([ADMIN_USER_NAME, ADMIN_USER_EMAIL]):
+    ADMINS += [(f"{ADMIN_USER_NAME}", f"{ADMIN_USER_EMAIL}")]
+    MANAGERS = ADMINS
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 LOCAL_CDN = BASE_DIR.parent / "local-cdn"
